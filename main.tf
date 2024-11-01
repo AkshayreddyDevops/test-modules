2e35fda27e7fb1f4b69f26abf67e20c8bf8aa458
akga0822 <akshay.gaddam@netcracker.com>
2024-11-01 19:24:19 +0530

    tools-infra

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 output "test"{
-  value = module.test.test_out
+  value = module.test["x"].test_out
 }
 
 module test{
   source = "./modules/test"
-  v1 = var.v["v1"]
+  v1 = var.v["x"]["v1"]
 }
 
 
 output "test_v3" {
-  value = module.test.v3
+  value = module.test["x"].v3
 }
