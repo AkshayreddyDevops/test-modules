4d07405ef4af2a8de8edd6e88934f0db6868d5d1
akga0822 <akshay.gaddam@netcracker.com>
2024-11-04 21:07:26 +0530

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# output "test"{
-#   value = module.test.test_out
-# }
+output "test"{
+  value = module.test.test_out
+}
 
 module test {
   source = "./modules/test"
@@ -11,6 +11,5 @@ module test {
 }
 
 
-output "test2" {
-  value = var.v
-}
+
+
