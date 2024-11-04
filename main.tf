8957450347454ef6dddb62dd30211f12968624c5
akga0822 <akshay.gaddam@netcracker.com>
2024-11-04 20:59:14 +0530

    test

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ output "test"{
 
 module test {
   source = "./modules/test"
-  for_each = var.v["x"]
+  for_each = var.v
   name1 = each.value["v1"]
   name2 = each.value["v2"]
   name3 = each.value["l1"]
