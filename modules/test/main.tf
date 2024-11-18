2d60fdd07bb9e1e9e648492ada56dc4c53ffaeac
akga0822 <akshay.gaddam@netcracker.com>
2024-11-18 17:16:08 +0530

    test

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,5 +1,11 @@
-variable "test-sub" {
-  default = var.test-sub-1
+resource "aws_eks_access_policy_association" "github-workstation-access" {
+  cluster_name  = var.test-sub-1
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::054037101799:role/gethub-runner-role"
+
+  access_scope {
+    type       = "cluster"
+  }
 }
 
 output "test-out" {
