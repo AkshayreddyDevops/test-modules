cbb7c141c487feff292c6bf146dceb2ea8ad5089
akga0822 <akshay.gaddam@netcracker.com>
2024-11-18 17:08:30 +0530

    test

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "t1"{
   source = "./modules/test"
-
+  test = var.test["test-sub-1"]
 }
 
 
-output "c2" {
-  value = var.test["test-sub-1"]["test-11"]["name"]
-}
+# output "c2" {
+#   value = var.test["test-sub-1"]["test-11"]["name"]
+# }
